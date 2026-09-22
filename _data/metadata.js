0a06d47ac008457021bd132c119cd8af88a724f1
small patatas <smallpatatas@patatas.ca>
2026-09-22 15:02:47 -0400

    favicon, description, SEO keywords

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 export default {
-	title: "CCIA-AICC",
+	title: "Canadian Civic Infrastructure Alliance | Alliance de l'Infrastructure Civile Canadienne",
 	url: "https://ccia-aicc.org/",
 	language: "en",
-	description: "Canadian Civic Infrastructure Alliance",
+	description: "Working with residents, public officials and policy experts to strengthen public capacity around major digital and energy infrastructure decisions.",
 	author: {
 		name: "Canadian Civic Infrastructure Alliance",
 		email: "contact@ccia-aicc.org",
